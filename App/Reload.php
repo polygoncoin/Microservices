d16c79c79632d11d5a8226cf49458eba67628efc
Ramesh Jangid <polygon.co.in@gmail.com>
2023-04-18 15:37:05 +0530

    changes

--- App/Reload.php
+++ App/Reload.php
@@ -1,32 +1,9 @@
 <?php
 namespace App;
 
-use App\HttpRequest;
-use App\HttpErrorResponse;
-use App\Connection;
-/*
-MIT License 
+use App\Authorize;
+use App\JsonEncode;
 
-Copyright (c) 2023 Ramesh Narayan Jangid. 
-
-Permission is hereby granted, free of charge, to any person obtaining a copy 
-of this software and associated documentation files (the "Software"), to deal 
-in the Software without restriction, including without limitation the rights 
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions: 
-
-The above copyright notice and this permission notice shall be included in all 
-copies or substantial portions of the Software. 
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
-SOFTWARE. 
-*/
 /**
  * Updates cache
  *
@@ -39,7 +16,7 @@ SOFTWARE.
  * @version    Release: @1.0.0@
  * @since      Class available since Release 1.0.0
  */
-class Reload extends HttpRequest
+class Reload extends Authorize
 {
     /**
      * Server connection object
@@ -71,17 +48,18 @@ class Reload extends HttpRequest
      *
      * @return void
      */
-    function process($refresh)
+    function process($refresh, $idsString = null)
     {
-        $ids = null;
-        foreach (explode(',', trim($_GET['ids'])) as $value) {
-            if (ctype_digit($value = trim($value))) {
-                $ids[] = (int)$value;
-            } else {
-                HttpErrorResponse::return404('Only integer values supported for ids.');
+        $ids = [];
+        if (!is_null($idsString)) {
+            foreach (explode(',', trim($idsString)) as $value) {
+                if (ctype_digit($value = trim($value))) {
+                    $ids[] = (int)$value;
+                } else {
+                    HttpErrorResponse::return404('Only integer values supported for ids.');
+                }
             }
         }
-        
         switch ($refresh) {
             case 'user':
                 $this->processUser($ids);
