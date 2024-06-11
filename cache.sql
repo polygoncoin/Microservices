0bf1de4fce701a9ccacab2041ff2723b02c6ebfd
“polygoncoin <polygon.co.in@gmail.com>
2024-06-11 21:17:56 +0530

    Refactoring

--- cache.sql
+++ cache.sql
@@ -1,91 +1,44 @@
--- MySQL dump 10.13  Distrib 8.0.32, for macos12.6 (x86_64)
---
--- Host: localhost    Database: cache
--- ------------------------------------------------------
--- Server version	8.0.32
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `user`
---
-
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `user` (
   `key` VARCHAR(255) NOT NULL,
   `value` TEXT DEFAULT NULL,
   `ts` int DEFAULT 0,
   KEY (`key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `group`
---
 
 DROP TABLE IF EXISTS `group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `group` (
   `key` INT NOT NULL,
   `value` TEXT DEFAULT NULL,
   `ts` int DEFAULT 0,
   KEY (`key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `cidr`
---
 
 DROP TABLE IF EXISTS `cidr`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `cidr` (
   `key` INT NOT NULL,
   `value` TEXT DEFAULT NULL,
   `ts` int DEFAULT 0,
   KEY (`key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `usertoken`
---
 
 DROP TABLE IF EXISTS `usertoken`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `usertoken` (
   `key` INT NOT NULL,
   `value` TEXT DEFAULT NULL,
   `ts` int DEFAULT 0,
   KEY (`key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `token`
---
 
 DROP TABLE IF EXISTS `token`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `token` (
   `key` varchar(255) NOT NULL,
   `value` TEXT DEFAULT NULL,
   `ts` int DEFAULT 0,
   KEY (`key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
--- global.sql
+++ global.sql
@@ -1,27 +1,5 @@
--- MySQL dump 10.13  Distrib 8.0.32, for macos12.6 (x86_64)
---
--- Host: localhost    Database: global
--- ------------------------------------------------------
--- Server version	8.0.32
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `m001_master_group`
---
-
 DROP TABLE IF EXISTS `m001_master_group`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `m001_master_group` (
   `group_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -40,15 +18,9 @@ CREATE TABLE `m001_master_group` (
   `is_deleted` enum('Yes','No') NOT NULL DEFAULT 'No',
   PRIMARY KEY (`group_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `m002_master_user`
---
 
 DROP TABLE IF EXISTS `m002_master_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `m002_master_user` (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
@@ -68,15 +40,9 @@ CREATE TABLE `m002_master_user` (
   `is_deleted` enum('Yes','No') NOT NULL DEFAULT 'No',
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `m003_master_connection`
---
 
 DROP TABLE IF EXISTS `m003_master_connection`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `m003_master_connection` (
   `connection_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255),
@@ -98,15 +64,9 @@ CREATE TABLE `m003_master_connection` (
   `is_deleted` enum('Yes','No') NOT NULL DEFAULT 'No',
   PRIMARY KEY (`connection_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `m004_master_client`
---
 
 DROP TABLE IF EXISTS `m004_master_client`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `m004_master_client` (
   `client_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -122,11 +82,6 @@ CREATE TABLE `m004_master_client` (
   `is_deleted` enum('Yes','No') NOT NULL DEFAULT 'No',
   PRIMARY KEY (`client_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `m001_master_group`
---
 
 LOCK TABLES `m001_master_group` WRITE;
 /*!40000 ALTER TABLE `m001_master_group` DISABLE KEYS */;
@@ -137,9 +92,6 @@ INSERT INTO `m001_master_group` VALUES
 /*!40000 ALTER TABLE `m001_master_group` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping data for table `m002_master_user`
---
 
 LOCK TABLES `m002_master_user` WRITE;
 /*!40000 ALTER TABLE `m002_master_user` DISABLE KEYS */;
@@ -150,10 +102,6 @@ INSERT INTO `m002_master_user` VALUES
 /*!40000 ALTER TABLE `m002_master_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping data for table `m003_master_connection`
---
-
 LOCK TABLES `m003_master_connection` WRITE;
 /*!40000 ALTER TABLE `m003_master_connection` DISABLE KEYS */;
 INSERT INTO `m003_master_connection` VALUES
@@ -163,10 +111,6 @@ INSERT INTO `m003_master_connection` VALUES
 /*!40000 ALTER TABLE `m003_master_connection` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping data for table `m004_master_client`
---
-
 LOCK TABLES `m004_master_client` WRITE;
 /*!40000 ALTER TABLE `m004_master_client` DISABLE KEYS */;
 INSERT INTO `m004_master_client` VALUES
@@ -175,14 +119,3 @@ INSERT INTO `m004_master_client` VALUES
 (3,'Client 002','',NULL,'2023-04-15 08:54:50',NULL,NULL,NULL,'2023-04-29 16:00:41','Yes','No','No');
 /*!40000 ALTER TABLE `m004_master_client` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-05-30 11:57:58
