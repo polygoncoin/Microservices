c2aebd1101868bf171b4dd1f15b6c5ea491febb9
“polygoncoin <polygon.co.in@gmail.com>
2024-09-29 17:11:28 +0530

    Seperated cache keys

--- App/CacheKey.php
+++ App/CacheKey.php
@@ -15,31 +15,68 @@ namespace Microservices\App;
  */
 class CacheKey
 {
+    /**
+     * Get Client Key
+     * 
+     * @param string $hostname
+     * @return string
+     */
     static public function Client($hostname)
     {
         return "c:{$hostname}";
     }
 
+    /**
+     * Get Client User Key
+     * 
+     * @param integer $clientId
+     * @param string  $username
+     * @return string
+     */
     static public function ClientUser($clientId, $username)
     {
         return "cu:{$clientId}:u:{$username}";
     }
 
+    /**
+     * Get Group Key
+     * 
+     * @param integer $clientId
+     * @return string
+     */
     static public function Group($groupId)
     {
         return "g:{$groupId}";
     }
 
+    /**
+     * Get Group CIDR Key
+     * 
+     * @param integer $groupId
+     * @return string
+     */
     static public function CIDR($groupId)
     {
         return "cidr:{$groupId}";
     }
 
+    /**
+     * Get Token Key
+     * 
+     * @param string $token
+     * @return string
+     */
     static public function Token($token)
     {
         return "t:{$token}";
     }
 
+    /**
+     * Get User Token Key
+     * 
+     * @param integer $userId
+     * @return string
+     */
     static public function UserToken($userId)
     {
         return "ut:{$userId}";
