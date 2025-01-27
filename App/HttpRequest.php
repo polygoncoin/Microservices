bc5841755060c54cb6f318dd3d6c55f8cd821232
“polygoncoin <polygon.co.in@gmail.com>
2025-01-27 09:35:27 +0530

    Refactoring

--- App/HttpRequest.php
+++ App/HttpRequest.php
@@ -412,39 +412,6 @@ class HttpRequest
     }
 
     /**
-     * Returns Start IP and End IP for a given CIDR
-     *
-     * @param  string $cidrs IP address range in CIDR notation for check
-     * @return array
-     */
-    public function cidrsIpNumber($cidrs)
-    {
-        $response = [];
-
-        foreach (explode(',', str_replace(' ', '', $cidrs)) as $cidr) {
-            if (strpos($cidr, '/')) {
-                list($cidrIp, $bits) = explode('/', str_replace(' ', '', $cidr));
-                $binCidrIpStr = str_pad(decbin(ip2long($cidrIp)), 32, 0, STR_PAD_LEFT);
-                $startIpNumber = bindec(str_pad(substr($binCidrIpStr, 0, $bits), 32, 0, STR_PAD_RIGHT));
-                $endIpNumber = $startIpNumber + pow(2, $bits) - 1;
-                $response[] = [
-                    'start' => $startIpNumber,
-                    'end' => $endIpNumber
-                ];
-            } else {
-                if ($ipNumber = ip2long($cidr)) {
-                    $response[] = [
-                        'start' => $ipNumber,
-                        'end' => $ipNumber
-                    ];
-                }
-            }
-        }
-
-        return $response;
-    }
-
-    /**
      * Set Cache
      *
      * @return void
