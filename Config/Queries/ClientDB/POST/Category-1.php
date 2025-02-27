4d957a855a5b04c4e4c24f852dc28501fa266927
“polygoncoin <polygon.co.in@gmail.com>
2025-02-27 14:54:17 +0530

    tests

---
+++ Config/Queries/ClientDB/POST/Category-1.php
@@ -0,0 +1,35 @@
+<?php
+namespace Microservices\Config\Queries\ClientDB\POST;
+
+use Microservices\App\Constants;
+use Microservices\App\DatabaseDataTypes;
+use Microservices\App\Env;
+
+return [
+    'query' => "INSERT INTO `category` SET __SET__",
+    '__SET__' => [
+        'name' => ['payload', 'name'],
+        'parent_id' => ['custom', 0],
+    ],
+    'insertId' => 'category:id',
+    'subQuery' => [
+        'sub' => [
+            'query' => "INSERT INTO `category` SET __SET__",
+            '__SET__' => [
+                'name' => ['payload', 'subname'],
+                'parent_id' => ['insertIdParams', 'category:id'],
+            ],
+            'insertId' => 'sub:id',
+            'subQuery' => [
+                'subsub' => [
+                    'query' => "INSERT INTO `category` SET __SET__",
+                    '__SET__' => [
+                        'name' => ['payload', 'subsubname'],
+                        'parent_id' => ['insertIdParams', 'sub:id'],
+                    ],
+                    'insertId' => 'subsub:id',
+                ]
+            ]
+        ]
+    ]
+];
--- public_html/test.php
+++ public_html/test.php
@@ -61,7 +61,7 @@ function trigger($method, $route, $header = [], $json = '')
 $response = [];
 echo '<pre>';
 
-$response[] = trigger('GET', '/reload', [], '');
+// $response[] = trigger('GET', '/reload', [], '');
 
 $res = trigger('POST', '/login', [], '{"username":"client_1_group_1_user_1", "password":"shames11"}');
 if ($res) {
@@ -70,7 +70,9 @@ if ($res) {
     $header = ["Authorization: Bearer {$token}"];    
 
     $response[] = trigger('GET', '/routes', $header, '');
-    $response[] = trigger('POST', '/category', $header, '[{"name":"ramesh0","sub":{"subname":"ramesh1","subsub":{"subsubname":"ramesh"}}},{"name":"ramesh1","sub":{"subname":"ramesh1","subsub":{"subsubname":"ramesh"}}}]');
+    $response[] = trigger('POST', '/category-1', $header, '[{"name":"ramesh0","subname":"ramesh1","subsubname":"ramesh2"},{"name":"ramesh0","subname":"ramesh1","subsubname":"ramesh2"}]');
+    $response[] = trigger('GET', '/category-1', $header, '');
+    $response[] = trigger('POST', '/category', $header, '[{"name":"ramesh0","sub":{"subname":"ramesh1","subsub":[{"subsubname":"ramesh"},{"subsubname":"ramesh"}]}},{"name":"ramesh1","sub":{"subname":"ramesh1","subsub":{"subsubname":"ramesh"}}}]');
     $response[] = trigger('GET', '/category', $header, '');
     $response[] = trigger('POST', '/category/config', $header, '');
 }
