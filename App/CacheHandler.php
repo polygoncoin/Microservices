666db7a4fbb0e04ff43b5db5d8aa34b18545391b
“polygoncoin <polygon.co.in@gmail.com>
2025-08-17 16:12:30 +0530

    Data Represntation

--- App/CacheHandler.php
+++ App/CacheHandler.php
@@ -110,7 +110,7 @@ class CacheHandler
 
         // Get the $fileLocation file mime
         $fileInfo = finfo_open(flags: FILEINFO_MIME_TYPE);
-        $mime = finfo_file(finfo: $fileInfo, filename: $fileLocation);
+        $mime = finfo_file(finfo: $fileInfo, filename: $this->_fileLocation);
         finfo_close(finfo: $fileInfo);
 
         // Let Etag be last modified timestamp of file
@@ -127,24 +127,26 @@ class CacheHandler
                 datetime: $_SERVER['HTTP_IF_MODIFIED_SINCE']
             ) == $modifiedTime)
         ) {
-            header('HTTP/1.1 304 Not Modified');
+            header(header: 'HTTP/1.1 304 Not Modified');
             return true;
         }
 
         // send the headers
         //header("Content-Disposition: attachment;filename='$fileName';");
-        header('Cache-Control: max-age=0, must-revalidate');
-        header("Last-Modified: ".gmdate(
-            format: "D, d M Y H:i:s",
-            timestamp: $modifiedTime)." GMT"
+        header(header: 'Cache-Control: max-age=0, must-revalidate');
+        header(
+            header: "Last-Modified: " . gmdate(
+                format: "D, d M Y H:i:s",
+                timestamp: $modifiedTime
+            ) . " GMT"
         );
-        header("Etag:\"{$eTag}\"");
-        header('Expires: -1');
-        header("Content-Type: $mime");
-        header('Content-Length: ' . filesize(filename: $fileLocation));
+        header(header: "Etag:\"{$eTag}\"");
+        header(header: 'Expires: -1');
+        header(header: "Content-Type: $mime");
+        header(header: 'Content-Length: ' . filesize(filename: $this->_fileLocation));
 
         // Send file content as stream
-        $fp = fopen(filename: $fileLocation, mode: 'rb');
+        $fp = fopen(filename: $this->_fileLocation, mode: 'rb');
         fpassthru(stream: $fp);
 
         return true;
