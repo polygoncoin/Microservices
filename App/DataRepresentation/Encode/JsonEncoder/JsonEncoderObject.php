a663d40b95ed8b4ceaeec78454b72af5c8fd8056
“polygoncoin <polygon.co.in@gmail.com>
2026-02-11 17:18:22 +0530

    Output PHP

--- App/DataRepresentation/Encode/JsonEncoder/JsonEncoderObject.php
+++ App/DataRepresentation/Encode/JsonEncoder/JsonEncoderObject.php
@@ -13,7 +13,7 @@
  * @since     Class available since Release 1.0.0
  */
 
-namespace Microservices\App\DataRepresentation\Encode\JsonEncode;
+namespace Microservices\App\DataRepresentation\Encode\JsonEncoder;
 
 /**
  * JSON object
