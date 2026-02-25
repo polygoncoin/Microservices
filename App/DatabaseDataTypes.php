6c0d48337f34084116734b32768540c63a38f768
“polygoncoin <polygon.co.in@gmail.com>
2026-02-25 20:13:58 +0530

    DocBlcck

--- App/DatabaseDataTypes.php
+++ App/DatabaseDataTypes.php
@@ -6,7 +6,7 @@
  *
  * @category  DataTypes
  * @package   Microservices
- * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
+ * @author    Ramesh N. Jangid (Sharma) <polygon.co.in@gmail.com>
  * @copyright © 2026 Ramesh N. Jangid (Sharma)
  * @license   MIT https://opensource.org/license/mit
  * @link      https://github.com/polygoncoin/Microservices
@@ -320,7 +320,7 @@ namespace Microservices\App;
  *
  * @category  DataTypes
  * @package   Microservices
- * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
+ * @author    Ramesh N. Jangid (Sharma) <polygon.co.in@gmail.com>
  * @copyright © 2026 Ramesh N. Jangid (Sharma)
  * @license   MIT https://opensource.org/license/mit
  * @link      https://github.com/polygoncoin/Microservices
